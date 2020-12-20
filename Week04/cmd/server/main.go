93410176ef4fc64fa6119c3255a20543c7f48605
李成杰 <2835347017@qq.com>
2020-12-20 23:56:05 +0800

    Go-000/Week04

---
+++ Week04/cmd/server/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	pb "Week04/api/user/v1"
+	"Week04/internal/biz"
+	"Week04/internal/data"
+	"Week04/internal/pkg/grpc"
+	"Week04/internal/service"
+	"github.com/google/wire"
+	"golang.org/x/sync/errgroup"
+)
+
+const (
+	address = ":9800"
+)
+
+func main() {
+	// init service api
+	us := InitUserUsecase()
+	service := service.NewUserService(us)
+
+	// register grpc service
+	s := grpc.NewServer(address)
+	pb.RegisterUserServer(s, service)
+
+	// context
+	ctx, cancel := context.WithCancel(context.Background())
+	g, ctx := errgroup.WithContext(ctx)
+
+	// start grpc server
+	g.Go(func() error {
+		return s.Start(ctx)
+	})
+
+	// catch signals
+	g.Go(func() error {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+		select {
+		case sig := <-sigs:
+			log.Printf("signal caught: %s, ready to quit...", sig.String())
+			cancel()
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+		return nil
+	})
+
+	// wait stop
+	if err := g.Wait(); err != nil {
+		log.Printf("error: %v", err)
+	}
+}
+
+func InitUserUsecase() *biz.UserUsecase {
+	wire.Build(biz.NewUserUsecase, data.NewUserRepo)
+	return &biz.UserUsecase{}
+}
+
+// Injectors from wire.go:
+
+func InitUserUsecase() *biz.UserUsecase {
+	userRepo := data.NewUserRepo()
+	userUsecase := biz.NewUserUsecase(userRepo)
+	return userUsecase
+}
